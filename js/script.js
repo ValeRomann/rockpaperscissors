8e993f77e6e21cc7b1525430c32c0c541329b9ca
valeroman <valeraromanovo@gmail.com>
2022-11-12 12:07:36 +0300

    Write plarRound function

--- js/script.js
+++ js/script.js
@@ -3,5 +3,19 @@ function getComputerChoice() {
     return arr[Math.floor(Math.random() * 3)];
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-console.log(getComputerChoice());
+    if (playerSelection === computerSelection) return "Nobody's win";
+    
+    if ((playerSelection === 'Paper' && computerSelection === 'Scissors') ||
+       (playerSelection === 'Rock' && computerSelection === 'Paper') ||
+       (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+        return "You Lose! " + computerSelection + " beats " + playerSelection; ;
+    }
+    return "You Win! " + playerSelection + " beats " + computerSelection;
+}
+
+let playerSelection = prompt("Input Rock or Paper or Scissors");
+
+console.log(playRound(playerSelection, getComputerChoice()));
