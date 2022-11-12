d59cb288d003e0154c190af6d0369ed56d5b8497
valeroman <valeraromanovo@gmail.com>
2022-11-12 12:47:24 +0300

    Complete script width game function

--- js/script.js
+++ js/script.js
@@ -6,16 +6,35 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-    if (playerSelection === computerSelection) return "Nobody's win";
     
+    if (playerSelection === computerSelection){ 
+        console.log("Nobody's win");
+        return 0;
+    }
     if ((playerSelection === 'Paper' && computerSelection === 'Scissors') ||
        (playerSelection === 'Rock' && computerSelection === 'Paper') ||
        (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
-        return "You Lose! " + computerSelection + " beats " + playerSelection; ;
+        console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        return -1;
     }
-    return "You Win! " + playerSelection + " beats " + computerSelection;
+    console.log("You Win! " + playerSelection + " beats " + computerSelection);
+    return 1;
 }
 
-let playerSelection = prompt("Input Rock or Paper or Scissors");
+function game(roundNum) {
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 0; i < roundNum; i++) {
+        let playerSelection = prompt("Input Rock or Paper or Scissors");
+        let currentRound = playRound(playerSelection, getComputerChoice());
+        if (currentRound === 1) playerScore++;
+        if (currentRound === -1) computerScore++;
+    }
+    if (playerScore > computerScore) return "You win! Your score: " + playerScore + ", Computer score: " + computerScore;
+    if (playerScore < computerScore) return "You lose! Your score: " + playerScore + ", Computer score: " + computerScore;
+    return "Nobody's win! Your score: " + playerScore + ", Computer score: " + computerScore;
+}
 
-console.log(playRound(playerSelection, getComputerChoice()));
+
+console.log(game(3));
