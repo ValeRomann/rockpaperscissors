7fc6f6e2f42f3fc95aedee0d1731324ca35f2e68
valeroman <valeraromanovo@gmail.com>
2022-11-12 11:25:32 +0300

    Make function getComputerChoice that returns rock or paper or scissors

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('kek');
+function getComputerChoice() {
+    let arr = ['Rock', 'Paper', 'Scissors'];
+    return arr[Math.floor(Math.random() * 3)];
+}
+
+
+console.log(getComputerChoice());
